0fe7d68bd893caf2926aefda6a2b6278357d4d61
Arif <faizin.arief@gmail.com>
2019-12-04 14:57:21 +0700

    Create option menu

--- app/src/main/java/com/dicoding/picodiploma/mybottomnavigation/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mybottomnavigation/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.dicoding.picodiploma.mybottomnavigation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -23,5 +26,19 @@ class MainActivity : AppCompatActivity() {
         ).build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            //aksi ketika menu diklik
+        } else if (item.itemId == R.id.action_search) {
+            Toast.makeText(this, getString(R.string.tap_search_menu), Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
